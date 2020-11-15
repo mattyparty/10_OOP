d19976434b170ed613b8a5e01d8983b4b148f8ed
Matty Party <matthewpewewardy@gmail.com>
2020-11-15 13:40:54 -0600

    bug fixes

--- app.js
+++ app.js
@@ -37,31 +37,6 @@ const employeeQuestions = [
   },
 ];
 
-////
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ``
-
 //manager function
 function manager() {
   console.log("Lets Build the Team!");
@@ -79,9 +54,82 @@ function manager() {
   });
 }
 //engineer funtion
-function engineer(){
-    //engineer function here
+function engineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "engineerName",
+        message: "Input  engineer's name?",
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "Input engineer's ID?",
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "Input engineer's email?",
+      },
+      {
+        type: "input",
+        name: "engineerGithub",
+        message: "Input engineer's GitHub username?",
+      },
+    ])
+    .then(function (data) {
+      const engineer = new Engineer(
+        data.engineerName,
+        data.engineerId,
+        data.engineerEmail,
+        data.engineerGithub
+      );
+      teamMembers.push(engineer);
+      emptyId.push(data.engineerId);
+      team();
+    });
 }
+
+//Intern Function
+
+function intern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "internName",
+        message: "Input intern's name?",
+      },
+      {
+        type: "input",
+        name: "internId",
+        message: "Input intern's ID?",
+      },
+      {
+        type: "input",
+        name: "internEmail",
+        message: "Input intern's email?",
+      },
+      {
+        type: "input",
+        name: "internSchool",
+        message: "Input intern's school?",
+      },
+    ])
+    .then(function (data) {
+      const intern = new Intern(
+        data.internName,
+        data.internId,
+        data.internEmail,
+        data.internSchool
+      );
+      teamMembers.push(intern);
+      emptyId.push(intern);
+      team();
+    });
+}
+
 function team() {
   //build team here
   inquirer
@@ -100,6 +148,16 @@ function team() {
         intern();
       } else outputTeam();
     });
+}
+
+//function to render html pages
+
+function outputTeam() {
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+  fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+  console.log(teamMembers);
 }
 
 // function to initialize program
